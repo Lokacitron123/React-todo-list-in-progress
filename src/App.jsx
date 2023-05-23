7efb4b86569ef5ab8becda1691a97d3dcdce4459
Johan Lindell <johan.lindell91@gmail.com>
2023-05-23 13:18:35 +0200

    Added search functiontionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,48 @@
 import Header from "./Header";
+import SearchItem from "./SearchItem";
+import AddItem from "./AddItem";
 import Content from "./Content";
 import Footer from "./Footer";
 import { useState } from "react";
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: true,
-      item: "One half pound bag of Cocoa Covered Almonds Unsalted",
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Item 2",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Item 3",
-    },
-  ]);
+  // states
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("shoppinglist"))
+  );
+
+  const [newItem, setNewItem] = useState("");
+  const [search, setSearch] = useState("");
+
+  // functions below
+
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(newItems));
+  };
+
+  // Add Item
+  // the initial ? after items checks if it is defined and not null
+  // If items is not null it'll check if items.length is true (greater than 0)
+  // If it is greater than 0 it accesses the id property of the last item in the items array and icrements it by 1 to generate a new id.
+  // If If items is null or items.length is false, it will assign 1 to the id for the new item instead
+
+  // const listItem creates a new array from the the spread operator
+  // (items ?? []) is used to provide an emptry array as a fallback value incase items is null or undefined
+  // myNewItem is an object added to the end of the listItems array
+  // the ?? operator works like this
+  // const result = a ?? b;
+  // If a is not null or undefined, the result will be the value of a.
+  // If a is null or undefined the result will be the value of b
+
+  // The reason for the ?? is to check if a value is initially null or undefined. Even it is null an array can be created or what ever code you want to run after ??
+
+  const addItem = (item) => {
+    const id = items?.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item };
+    const listItems = [...(items ?? []), myNewItem];
+    setAndSaveItems(listItems);
+  };
 
   const handleCheck = (id) => {
     // 1. We use map which creates a new array
@@ -34,27 +56,44 @@ function App() {
     );
 
     // we use setItems to set a new state
-    setItems(listItems);
+    setAndSaveItems(listItems);
     // Saving setItem to localstorage
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
   };
 
-  //
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+    setAndSaveItems(listItems);
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    addItem(newItem);
+    // resets newItem with setNewItem after submission by setting it to an emptry string again
+    setNewItem("");
+  };
+
+  // length checker
+  const itemCount = items ? items.length : 0;
 
   return (
     <div className="App">
-      <Header />
+      <Header title={"Grocery List"} />
+
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <SearchItem search={search} setSearch={setSearch} />
       <Content
-        items={items}
+        items={items.filter((item) =>
+          item.item.toLowerCase().includes(search.toLocaleLowerCase())
+        )}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
-      <Footer length={items.length} />
+      <Footer length={itemCount} />
     </div>
   );
 }
--- src/Content.jsx
+++ src/Content.jsx
@@ -2,12 +2,13 @@ import ItemList from "./ItemList";
 
 // Prop drilling by deconstructing incoming props date from App.jsx
 const Content = ({ items, handleCheck, handleDelete }) => {
-  // ? :  = turnary conditional operator items.length is being checked and if is a length this return will jump into the UL, but if not it will show a paragraph.
-  // the formula goes like this a ? b : c
-  // If A is true true => B, if A is false => C
+  // The error handling goes like this
+  // (1) Check if items is true (not null or undefined)
+  // (2) Checks if items.length exists
+  // If both are true, it'll render the ItemList, otherwire the p tag will render saying your list is empty
   return (
     <main>
-      {items.length ? (
+      {items && items.length ? (
         <ItemList
           items={items}
           handleCheck={handleCheck}
